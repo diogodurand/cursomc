f7421be65c051ee9e60235abce6036d0429a9809
Diogo Durand <diogodurand_1@hotmail.com>
2019-01-28 21:37:39 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/diogodurand/cursomc/repositorioes/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.diogodurand.cursomc.repositorioes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.diogodurand.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
