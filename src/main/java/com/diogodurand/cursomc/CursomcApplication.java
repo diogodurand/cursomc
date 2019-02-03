280611d73b7baf9e2578425e1fc23fb4bdf4d5ad
Diogo Durand <diogodurand_1@hotmail.com>
2019-02-03 00:05:54 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/diogodurand/cursomc/CursomcApplication.java
+++ src/main/java/com/diogodurand/cursomc/CursomcApplication.java
@@ -69,6 +69,11 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... pArgs) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Categoria 3");
+		Categoria cat4 = new Categoria(null, "Categoria 4");
+		Categoria cat5 = new Categoria(null, "Categoria 5");
+		Categoria cat6 = new Categoria(null, "Categoria 6");
+		Categoria cat7 = new Categoria(null, "Categoria 7");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -81,7 +86,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2, cat3, cat4, cat5, cat6 , cat7));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
