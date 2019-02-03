7b548df5208e39a0bbe7fa90474435b8fde4504a
Diogo Durand <diogodurand_1@hotmail.com>
2019-02-03 01:15:04 -0300

    correcoes UPDATE em CategoriaService e construtor de Pagamento

--- src/main/java/com/diogodurand/cursomc/services/CategoriaService.java
+++ src/main/java/com/diogodurand/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.diogodurand.cursomc.domain.Categoria;
+import com.diogodurand.cursomc.domain.Cliente;
 import com.diogodurand.cursomc.dto.CategoriaDTO;
 import com.diogodurand.cursomc.repositorioes.CategoriaRepository;
 import com.diogodurand.cursomc.services.exceptions.DataIntegrityException;
@@ -34,8 +35,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -58,5 +60,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
