71b37573e730c2ffdac5d172de100f3cb6dd43d5
Durand <diogo.ventura.durand@accenture.com>
2019-02-05 15:22:56 -0300

    validacao customizado email nao repetido na atualizacao de cliente

--- src/main/java/com/diogodurand/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/diogodurand/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.diogodurand.cursomc.domain.Cliente;
+import com.diogodurand.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
