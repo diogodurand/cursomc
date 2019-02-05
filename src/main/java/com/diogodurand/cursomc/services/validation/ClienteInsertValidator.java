c48fdf91ae21872b4fa3649b9f0a47ae5a468f77
Durand <diogo.ventura.durand@accenture.com>
2019-02-05 15:04:55 -0300

    Validacao customiada: email nao repetido na insercao de cliente

--- src/main/java/com/diogodurand/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/diogodurand/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.diogodurand.cursomc.domain.Cliente;
 import com.diogodurand.cursomc.domain.enums.TipoCliente;
 import com.diogodurand.cursomc.dto.ClienteNewDTO;
+import com.diogodurand.cursomc.repositorioes.ClienteRepository;
 import com.diogodurand.cursomc.resources.exceptions.FieldMessage;
 import com.diogodurand.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 
@@ -21,6 +28,8 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
@@ -28,6 +37,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		if (aux != null) {
+			list.add(new FieldMessage("Email", "Email já existente"));
 		}
 
 		// inclua os testes aqui, inserindo erros na lista
