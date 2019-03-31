56d7bdae327ba1675ccefd16d1c2b67020a2a3e6
diogo.ventura.durand <diogo.ventura.durand@CPX-1B73FWFGMNN.dir.svc.accenture.com>
2019-03-31 11:34:58 -0300

    Email HTML

--- src/main/java/com/diogodurand/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/diogodurand/cursomc/services/AbstractEmailService.java
@@ -2,20 +2,44 @@ package com.diogodurand.cursomc.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.diogodurand.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
-	
+
 	@Value("${default.sender}")
 	private String sender;
+
+	@Autowired
+	private TemplateEngine templateEngine;
+
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
+	}
+
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		try {
+			MimeMessage mm = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mm);
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
 	}
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
@@ -28,4 +52,20 @@ public abstract class AbstractEmailService implements EmailService {
 		return sm;
 	}
 
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context ctx = new Context();
+		ctx.setVariable("pedido", obj);
+		return templateEngine.process("email/comfirnacaoPedido.html", ctx);
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mm = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mm, true);
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido Confirmado, Código: " + obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(obj), true);
+		return mm;
+	}
 }
